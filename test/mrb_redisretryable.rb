d74462a6528cb0a3a4a55d836a11868536475130
k1low <k1low@pepabo.com>
2018-03-27 12:04:19 +0900

    Add re-Auth

--- test/mrb_redisretryable.rb
+++ test/mrb_redisretryable.rb
@@ -3,7 +3,8 @@
 ##
 
 HOST         = "127.0.0.1"
-PORT         = 6999
+PORT         = 6998
+SECURED_PORT = 6999
 
 assert("Redis.new can not reconnect") do
   # 1. Start redis-server
@@ -98,4 +99,40 @@ assert("If Redis is stopped, Redis::Retryable raise Redis::Retryable::RetryError
   rescue => e
     assert_equal Redis::Retryable::RetryError, e.class
   end
+end
+
+assert("Redis::Retryable.new can re-auth") do
+  # 1.
+  pid = Process.fork() {
+    Exec.execv("/bin/bash", "-l", "-c", "redis-server --port #{SECURED_PORT} --requirepass 'secret' &")
+  }
+  Process.waitpid pid
+  Sleep::sleep(1)
+
+  # 2.
+  r = Redis::Retryable.new HOST, SECURED_PORT
+  assert_equal r.auth("secret"), "OK"
+  assert_equal "PONG", r.ping
+
+  # 3.
+  pid = Process.fork() {
+    Exec.execv("/bin/bash", "-l", "-c", "redis-cli -p #{SECURED_PORT} -a secret shutdown")
+  }
+  Process.waitpid pid
+  Sleep::sleep(1)
+  pid = Process.fork() {
+    Exec.execv("/bin/bash", "-l", "-c", "redis-server --port #{SECURED_PORT} --requirepass 'secret' &")
+  }
+  Process.waitpid pid
+  Sleep::sleep(1)
+
+  # 4.
+  assert_equal "PONG", r.ping
+  r.close
+
+  pid = Process.fork() {
+    Exec.execv("/bin/bash", "-l", "-c", "redis-cli -p #{SECURED_PORT} -a secret shutdown")
+  }
+  Process.waitpid pid
+  Sleep::sleep(1)
 end
